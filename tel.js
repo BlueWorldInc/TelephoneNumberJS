09a05952a49a4b6f1b7341801843b3e14ddc2014
BlueWorldInc <keamybakunin@gmail.com>
2020-01-18 13:41:35 +0100

    [add] the tree insertion is not working correctly

--- tel.js
+++ tel.js
@@ -1,12 +1,13 @@
 //t = [ '0467123456' ]              // cas 1
-t = [ '0123456789', '1123456789' ]; // cas 2
+t = [ '0123456789', '0123457789' ]; // cas 2
 
 class Arbre {
   constructor(n) {
     this.number = n;
+    this.son = [];
   }
   addSon(s) {
-    this.son = s;  
+    this.son.push(s);
   }
 }
 
@@ -22,7 +23,43 @@ for (var i = 1; i < t[0].length; i++) {
     oldArbre = newArbre;
 }
 
+// search in the tree
+var d = 0;
 
+searchInTree(arbreList[0], t[1], d);
+
+function searchInTree(tree, tel, depth, parentTree) {
+    if (tel.charAt(depth) == tree.number) {
+        console.log("the same tel number");
+        console.log(depth);
+        if (++depth < tel.length) {
+            searchInTree(tree.son[0], tel, depth, tree);
+        }
+    } else {
+        console.log("not the same tel number");
+        //console.log(tree);
+        createNewArbre(parentTree, tel, depth);
+    }
+}
+
+function createNewArbre(parent, telToAdd, depth) {
+    newArbre = new Arbre(telToAdd.charAt(depth));
+    console.log(parent);
+    console.log(depth);
+    parent.addSon(newArbre);
+    if (++depth < telToAdd.length) {
+        createNewArbre(parent.son[0], telToAdd, depth);
+    }
+}
+
+/*
+for (var i = 0; i < t[1].length; i++) {
+    if (t[1].charAt(i) == arbreList[0].number)
+
+
+}
+*/
+/*
 arbreList.push(new Arbre(t[1].charAt(0)));
 var oldArbre = arbreList[1];
 
@@ -31,5 +68,5 @@ for (var i = 1; i < t[1].length; i++) {
     oldArbre.addSon(newArbre);
     oldArbre = newArbre;
 }
-
+*/
 console.log(arbreList);
