d5967822ff166f020e7f70c46b7100f4ba104ea7
BlueWorldInc <keamybakunin@gmail.com>
2020-01-18 13:01:57 +0100

    [add] deleted comment

--- tel.js
+++ tel.js
@@ -1,10 +1,3 @@
-/*const N = parseInt(readline());
-var t = [];
-for (let i = 0; i < N; i++) {
-    const telephone = readline();
-    t.push(telephone);
-}
-*/
 //t = [ '0467123456' ]              // cas 1
 t = [ '0123456789', '1123456789' ]; // cas 2
 
@@ -19,32 +12,30 @@ class Arbre {
 
 console.log(t);
 
-//mycar = new Arbre(7);
-//myToyota = new Arbre(15);
-//mycar.son = myToyota;
-//mycar.addSon(myToyota);
-//console.log(mycar.son);
-//console.log(telephoneNumber[0]);
-
-var newArbreL = [];
-newArbreL.push(new Arbre(t[0].charAt(0)));
-var oldArbre = newArbreL[0];
+var arbreList = [];
+arbreList.push(new Arbre(t[0].charAt(0)));
+var oldArbre = arbreList[0];
 
 for (var i = 1; i < t[0].length; i++) {
     newArbre = new Arbre(t[0].charAt(i));
     oldArbre.addSon(newArbre);
-    newArbreL.push(newArbre);
+    arbreList.push(newArbre);
     oldArbre = newArbre;
 }
 
-newArbreL.push(new Arbre(t[1].charAt(0)));
-var oldArbre = newArbreL[newArbreL.length -1];
+
+
+
+
+
+arbreList.push(new Arbre(t[1].charAt(0)));
+var oldArbre = arbreList[arbreList.length -1];
 
 for (var i = 1; i < t[1].length; i++) {
     newArbre = new Arbre(t[1].charAt(i));
     oldArbre.addSon(newArbre);
-    newArbreL.push(newArbre);
+    arbreList.push(newArbre);
     oldArbre = newArbre;
 }
 
-console.log(newArbreL);
+console.log(arbreList);
